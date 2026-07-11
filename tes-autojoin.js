3fca1accbf302575e363cd5db68887faeafbb6d1
PaxBar-APKBUG <angsagacor671@gmail.com>
2026-07-11 12:12:36 +0800

    Admin ubah status absen user: Radzz

--- tes-autojoin.js
+++ tes-autojoin.js
@@ -7,7 +7,7 @@
       "whatsapp_number": "6285850493138",
       "username": "Radzz",
       "max_id": 10,
-      "status_absens": "BELUM_SHARE",
+      "status_absens": "SUDAH_SHARE",
       "links": {
         "channels": [],
         "groups": []
