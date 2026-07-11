148993a20258a72e07f6e92a4bfc4e56acbba6df
PaxBar-APKBUG <angsagacor671@gmail.com>
2026-07-11 12:11:34 +0800

    Admin tambah user baru: Radzz

--- tes-autojoin.js
+++ tes-autojoin.js
@@ -1,1 +1,17 @@
-[]
+{
+  "settings": {
+    "global_share_time": "15:00-22:00"
+  },
+  "users": [
+    {
+      "whatsapp_number": "6285850493138",
+      "username": "Radzz",
+      "max_id": 10,
+      "status_absens": "BELUM_SHARE",
+      "links": {
+        "channels": [],
+        "groups": []
+      }
+    }
+  ]
+}
